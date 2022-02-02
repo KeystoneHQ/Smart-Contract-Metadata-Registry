ffa618b6ce3014fc1726252643ad0d616aec03df
aaronisme <aarondongchen@gmail.com>
2022-02-02 11:56:06 +0800

    refactor the script code

--- merge_contracts_to_sqlite.py
+++ merge_contracts_to_sqlite.py
@@ -67,8 +67,8 @@ def get_contract_info(contract_path):
             content_address = content["address"].lower()
             contract_name = content["name"]
             contract_metadate = json.dumps(content["metadata"])
-            contract_version = int(content["version"])
-            contract_checkPoints = json.dumps(content["checkPoints"])
+            contract_version = int(content.get("version", 1))
+            contract_checkPoints = json.dumps(content.get("checkPoints",[]))
         except:
             raise
 
