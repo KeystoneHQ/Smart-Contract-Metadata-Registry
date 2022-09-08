6b43e2e3d8f34fab441b76916707c41bce9cf0ef
NanYeZhuYu <102504705+NanYeZhuYu@users.noreply.github.com>
2022-09-08 11:00:20 +0800

    fix check_file.py
    supplementary verification when Field abi does not exist

--- check_file.py
+++ check_file.py
@@ -18,11 +18,9 @@ def check_file(path):
         contract_json = json.load(f)
         if(contract_json.get('name') is None or contract_json.get('chainId') is None or contract_json.get('address') is None or contract_json.get('metadata') is None or contract_json.get('version') is None):
             raise MissingFieldError('Require Field missing, name,chainId,address,metadata,version is required, please check the read me file')
-        
-        try:
-            contract_json.get('metadata').get('output').get('abi')
-        except AttributeError as e:
-            raise MetadataFieldError('Metadata field verfication error: please check the readme file')
+
+        if(contract_json.get('metadata').get('output') is None or contract_json.get('metadata').get('output').get('abi') is None):
+            raise MissingFieldError('Require Field missing, metadata.output.abi is required, please check the read me file')
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
